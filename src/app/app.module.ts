395ba6d78ebb2cc6b1f9e73b21300498dacfc548
Jhon Jairo Vargas Vargas <vargas.jhon@gmail.com>
2023-07-13 18:55:20 -0500

    [+] ng g c components/home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HOMEComponent } from './component/home/home.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HOMEComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
