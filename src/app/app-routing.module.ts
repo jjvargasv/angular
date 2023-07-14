9efd60679c403a1b1b703300ecb222328c25c333
Jhon Jairo Vargas Vargas <vargas.jhon@gmail.com>
2023-07-13 20:17:33 -0500

    [+] se crean las rutas de de las paguinas de home , logi , register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
 
-const routes: Routes = [];
+//localhost:4200
+const routes: Routes = [                                      // ->path page
+    { path: '', component: HomeComponent},                    // -> / home page  localhost:4200/
+    { path: 'login', component: LoginComponent},              // -> / login page  localhost:4200/login
+    { path: 'register', component: RegisterComponent},        // -> / register page  localhost:4200/register
+    { path: '**', redirectTo: ''},                            // -> / hola   page  localhost:4200/hola
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HOMEComponent } from './component/home/home.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
@@ -14,7 +13,6 @@ import { FooterComponent } from './shared/footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HOMEComponent,
     HomeComponent,
     LoginComponent,
     RegisterComponent,
