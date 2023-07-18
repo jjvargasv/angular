48dd04e4092220b77026104d5e23308c18ca685f
Jhon Jairo Vargas Vargas <vargas.jhon@gmail.com>
2023-07-17 20:09:30 -0500

    [+] inportamos el reactiveFormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+
 
 
 
@@ -21,7 +24,8 @@ import { FooterComponent } from './shared/footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  // Atributos de la clase
+  loginForm:FormGroup = this.fb.group({ 
+    email: [
+      '',     // valor predeterminado del campo
+      [     // validaciones
+        Validators.required,
+        Validators.email
+      ]
+    ],
+    password: [ 
+      '',     // valor predeterminado del campo
+      [       // validaciones
+        Validators.required,
+        Validators.minLength( 8 )
+      ]
+    ]
+    
+  });
+  /** Inyeccion de dependencias */
+  constructor( private fb : FormBuilder ) {}
 
+  /**Metodo: login */
+login(){
+    console.group('loginForm');
+    console.log(this.loginForm.value);
+    console.log(this.loginForm.value);
+    console.groupEnd();
+  }
 }
